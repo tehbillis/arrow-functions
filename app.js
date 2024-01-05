0007b12744f04092670bc66f305de543556b8272
WILLIAM GREEN <TEHBILLIS@GMAIL.COM>
2024-01-04 20:25:55 -0800

    Complete step 4

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ console.log('Without parens:', withoutParens('Code 301'));
 const oneLiner = course => `I cam currently enrolled in ${course}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('As a one-liner:', oneLiner('Code 301'));
+console.log('As a one-liner:', oneLiner('Code 301'));
 
 
 // STEP 5
